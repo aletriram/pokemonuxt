a79afa42009a4ffc9b7c98dd4a0f31346968776f
Alejandro Trinidad Ramos <35463094+aletriram@users.noreply.github.com>
2020-03-02 22:03:12 +0100

    componente listado, paginación (de 1 en 1)

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,13 +1,13 @@
 <template>
 	<div>
 		<b-navbar toggleable="lg" type="dark" variant="info">
-			<b-navbar-brand href="#">Pokemon Nuxt</b-navbar-brand>
+			<b-navbar-brand href="/">Pokemon nuxt</b-navbar-brand>
 
 			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
 			<b-collapse id="nav-collapse" is-nav>
 				<b-navbar-nav>
-					<b-nav-item href="#">Enlcejemplo</b-nav-item>
+					<b-nav-item href="/pokemons">Pokemons</b-nav-item>
 				</b-navbar-nav>
 
 				<!-- Right aligned nav items -->
---
+++ pages/pokemons/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<b-container>
+		<b-row>
+			<b-col>
+				<listado :pokemons="pokemons"></listado>
+			</b-col>
+		</b-row>
+		<b-row>
+			<b-col cols="auto" class="mr-auto"><b-button v-if="anterior" @click="getAnterior()">Anterior</b-button></b-col>
+			<b-col cols="auto" ><b-button v-if="siguiente" @click="getSiguiente()">Siguiente</b-button></b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<script>
+
+import axios from 'axios'
+import listado from '@/components/pokemons/listado.pokemons'
+
+export default {
+	components: {
+		listado
+	},
+	data() {
+		return {
+			pokemons: [],
+		}
+	},
+	watchQuery: ['page'],
+	asyncData ({isDev, route, store, env, params, query, req, res, redirect, error}) {
+		let page = query.page ? query.page : 1;
+		let limit = 10;
+		let offset = (page - 1) * limit; 
+
+		return axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`).then((res) => {
+			return { pokemons: res.data.results, anterior: res.data.previous || '', siguiente: res.data.next || '', page: page };
+		}).catch((e) => {
+			error({ statusCode: 404, message: 'Page not found' })
+		})
+	},
+	methods: {
+		getAnterior() {
+			this.$router.push({ path: '/pokemons', query: { page: --this.page }});
+		},
+		getSiguiente() {
+			this.$router.push({ path: '/pokemons', query: { page: ++this.page }});
+		}
+	}
+};
+</script>
